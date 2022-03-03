829822938d4c44e0411a23d5deb16f37f38280d8
Faculdade SENAI <fatec.senai@acad692.com.br>
2022-03-02 22:31:36 -0300

    aula 2

--- modulo-usuario/src/main/java/br/com/mbs/controller/UsuarioController.java
+++ modulo-usuario/src/main/java/br/com/mbs/controller/UsuarioController.java
@@ -1,24 +1,24 @@
 package br.com.mbs.controller;
 
 
-
-import java.util.Date;
-import java.util.List;
-import org.camunda.bpm.engine.RuntimeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.mbs.entidades.Usuario;
 import io.swagger.annotations.Api;
 
 @RestController(value="API para manipulacao de usuarios")
+@RequestMapping("usuarios")
 @Api(description="Api de usuarios")
 public class UsuarioController {
 
-	
+	Map<Integer,Usuario> mapaUsuario = new HashMap<Integer,Usuario>();
+	Integer contador = 1;
 	
 	 @RequestMapping(value = "/teste/", method = RequestMethod.GET)	 
 	  public ResponseEntity<Void> teste() throws Exception {		 
@@ -26,6 +26,42 @@ public class UsuarioController {
 		 return null;
 	  }
 	 
+	 
+	 @RequestMapping(value = "/salvar/", method = RequestMethod.POST)	 
+	  public ResponseEntity<Integer> salvarUsuario(
+			  
+			  @RequestParam("nome") String nome,
+			  @RequestParam("cep") String cep,
+			  @RequestParam("idade") Integer idade)
+	  
+			   throws Exception {
+		 
+		 System.out.println("Processando salvarUsuario");
+		 
+		 Usuario usuario = new Usuario();
+		 usuario.cep = cep;
+		 usuario.idade = idade;
+		 usuario.nome = nome;
+		 usuario.id = contador;
+		 		 
+		 mapaUsuario.put(contador, usuario);
+		 
+		 contador++;
+		 
+		 return ResponseEntity.ok(usuario.id);
+	  }
+	 
+	 
+	 @RequestMapping(value = "/buscar/{id}", method = RequestMethod.GET)	 
+	  public ResponseEntity<Usuario> buscar(
+			  
+			  @PathVariable("id") Integer id)
+			  
+			  throws Exception {		 
+		 
+		 System.out.println("Processando buscar ");
+		 return ResponseEntity.ok(mapaUsuario.get(id));
+	  }
 	
 
 }
---
+++ modulo-usuario/src/main/java/br/com/mbs/entidades/Usuario.java
@@ -0,0 +1,10 @@
+package br.com.mbs.entidades;
+
+
+public class Usuario {
+	
+	public Integer id;
+	public String nome;
+	public String cep;
+	public Integer idade;
+}
