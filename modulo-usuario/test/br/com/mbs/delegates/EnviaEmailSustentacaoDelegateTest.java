32a92deafd520c3b564bd71c007e240a0875ee15
professor <professor@UO692M4092251.acad692.com.br>
2022-04-20 21:52:48 -0300

    aula 9

---
+++ modulo-usuario/test/br/com/mbs/delegates/EnviaEmailSustentacaoDelegateTest.java
@@ -0,0 +1,14 @@
+package br.com.mbs.delegates;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EnviaEmailSustentacaoDelegateTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
